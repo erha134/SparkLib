c65ab33df589fc18fd93e82c2a90a566f03bc0a3
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2025-01-07 19:44:00 +0800

    更好的 `RegistrySupplier`

--- common/src/main/java/io/github/erha134/mc/sparklib/registry/impl/BlockRegistrySupplier.java
+++ common/src/main/java/io/github/erha134/mc/sparklib/registry/impl/BlockRegistrySupplier.java
@@ -1,11 +1,19 @@
 package io.github.erha134.mc.sparklib.registry.impl;
 
+import dev.architectury.registry.CreativeTabRegistry;
+import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
+import dev.architectury.registry.client.rendering.RenderTypeRegistry;
+import dev.architectury.registry.fuel.FuelRegistry;
 import dev.architectury.registry.registries.RegistrySupplier;
 import io.github.erha134.mc.sparklib.block.BlockProvider;
 import io.github.erha134.mc.sparklib.item.ItemProvider;
 import io.github.erha134.mc.sparklib.registry.api.DoubleWrappedRegisterSupplier;
 import net.minecraft.block.Block;
+import net.minecraft.client.color.block.BlockColorProvider;
+import net.minecraft.client.color.item.ItemColorProvider;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 
 public class BlockRegistrySupplier<B extends Block, I extends Item>
         extends DoubleWrappedRegisterSupplier<B, I>
@@ -22,5 +30,30 @@ public class BlockRegistrySupplier<B extends Block, I extends Item>
     @Override
     public Item asItem() {
         return this.delegate2.get();
+    }
+
+    public BlockRegistrySupplier<B, I> group(RegistrySupplier<ItemGroup> group) {
+        CreativeTabRegistry.append(group, this);
+        return this;
+    }
+
+    public BlockRegistrySupplier<B, I> fuel(int tick) {
+        FuelRegistry.register(tick, this);
+        return this;
+    }
+
+    public BlockRegistrySupplier<B, I> itemColor(ItemColorProvider color) {
+        ColorHandlerRegistry.registerItemColors(color, this);
+        return this;
+    }
+
+    public BlockRegistrySupplier<B, I> blockColor(BlockColorProvider color) {
+        ColorHandlerRegistry.registerBlockColors(color, this.sparklib$asBlock());
+        return this;
+    }
+
+    public BlockRegistrySupplier<B, I> renderLayer(RenderLayer layer) {
+        RenderTypeRegistry.register(layer, this.sparklib$asBlock());
+        return this;
     }
 }
--- common/src/main/java/io/github/erha134/mc/sparklib/registry/impl/ItemRegistrySupplier.java
+++ common/src/main/java/io/github/erha134/mc/sparklib/registry/impl/ItemRegistrySupplier.java
@@ -1,9 +1,14 @@
 package io.github.erha134.mc.sparklib.registry.impl;
 
+import dev.architectury.registry.CreativeTabRegistry;
+import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
+import dev.architectury.registry.fuel.FuelRegistry;
 import dev.architectury.registry.registries.RegistrySupplier;
 import io.github.erha134.mc.sparklib.item.ItemProvider;
 import io.github.erha134.mc.sparklib.registry.api.WrappedRegistrySupplier;
+import net.minecraft.client.color.item.ItemColorProvider;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 
 public class ItemRegistrySupplier<I extends Item> extends WrappedRegistrySupplier<I> implements ItemProvider {
     public ItemRegistrySupplier(RegistrySupplier<I> delegate) {
@@ -13,5 +18,20 @@ public class ItemRegistrySupplier<I extends Item> extends WrappedRegistrySupplie
     @Override
     public Item asItem() {
         return this.delegate.get();
+    }
+
+    public ItemRegistrySupplier<I> group(RegistrySupplier<ItemGroup> group) {
+        CreativeTabRegistry.append(group, this);
+        return this;
+    }
+
+    public ItemRegistrySupplier<I> fuel(int tick) {
+        FuelRegistry.register(tick, this);
+        return this;
+    }
+
+    public ItemRegistrySupplier<I> color(ItemColorProvider color) {
+        ColorHandlerRegistry.registerItemColors(color, this);
+        return this;
     }
 }
