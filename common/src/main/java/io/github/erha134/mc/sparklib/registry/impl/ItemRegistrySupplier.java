3a890a8b39070c51fa6552fc2be0f8fcc5b61fda
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2025-01-11 13:33:58 +0800

    添加 `ItemDeferredRegister#modelPredicate`

--- common/src/main/java/io/github/erha134/mc/sparklib/registry/impl/ItemRegistrySupplier.java
+++ common/src/main/java/io/github/erha134/mc/sparklib/registry/impl/ItemRegistrySupplier.java
@@ -3,14 +3,17 @@ package io.github.erha134.mc.sparklib.registry.impl;
 import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.client.rendering.ColorHandlerRegistry;
 import dev.architectury.registry.fuel.FuelRegistry;
+import dev.architectury.registry.item.ItemPropertiesRegistry;
 import dev.architectury.registry.registries.RegistrySupplier;
 import io.github.erha134.mc.sparklib.item.ItemProvider;
 import io.github.erha134.mc.sparklib.registry.api.WrappedRegistrySupplier;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.color.item.ItemColorProvider;
+import net.minecraft.client.item.ClampedModelPredicateProvider;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
 
 public class ItemRegistrySupplier<I extends Item> extends WrappedRegistrySupplier<I> implements ItemProvider {
     public ItemRegistrySupplier(RegistrySupplier<I> delegate) {
@@ -35,6 +38,12 @@ public class ItemRegistrySupplier<I extends Item> extends WrappedRegistrySupplie
     @Environment(EnvType.CLIENT)
     public ItemRegistrySupplier<I> color(ItemColorProvider color) {
         ColorHandlerRegistry.registerItemColors(color, this.get());
+        return this;
+    }
+
+    @Environment(EnvType.CLIENT)
+    public ItemRegistrySupplier<I> modelPredicate(Identifier property, ClampedModelPredicateProvider provider) {
+        ItemPropertiesRegistry.register(this.get(), property, provider);
         return this;
     }
 }
