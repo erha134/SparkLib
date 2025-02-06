cbb9c0f212e205d43d035a0180b22bb7acd08c9b
Mr_limr267 <113236317+erha134@users.noreply.github.com>
2025-02-06 22:15:00 +0800

    bye Custom Shield API

--- common/src/main/java/io/github/erha134/mc/sparklib/SparkLib.java
+++ common/src/main/java/io/github/erha134/mc/sparklib/SparkLib.java
@@ -1,16 +1,7 @@
 package io.github.erha134.mc.sparklib;
 
-import dev.architectury.event.EventResult;
-import dev.architectury.event.events.common.EntityEvent;
 import dev.architectury.platform.Platform;
-import io.github.erha134.mc.sparklib.item.shield.DamageableShield;
-import io.github.erha134.mc.sparklib.recipe.SRecipeSerializers;
-import io.github.erha134.mc.sparklib.recipe.SRecipeTypes;
 import io.github.erha134.mc.sparklib.util.VersionChecker;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,22 +15,6 @@ public final class SparkLib {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
     public static void init() {
-        SRecipeTypes.register();
-        SRecipeSerializers.register();
-
-        EntityEvent.LIVING_HURT.register((target, damageSource, amount) -> {
-            if (target instanceof PlayerEntity player) {
-                ItemStack stack = player.getActiveItem();
-                Entity attackSource = damageSource.getSource();
-
-                if (!stack.isEmpty() && stack.getItem() instanceof DamageableShield shield && attackSource instanceof LivingEntity livingEntity) {
-                    shield.damageAttacker(stack, player, livingEntity, amount);
-                }
-            }
-
-            return EventResult.pass();
-        });
-
         if (Platform.isDevelopmentEnvironment()) {
             CompletableFuture.supplyAsync(() -> VersionChecker.doCheck("47.3.22",
                             "https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json"))
--- common/src/main/java/io/github/erha134/mc/sparklib/item/shield/DamageableShield.java
+++
@@ -1,9 +0,0 @@
-package io.github.erha134.mc.sparklib.item.shield;
-
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-
-public interface DamageableShield {
-    void damageAttacker(ItemStack stack, PlayerEntity player, LivingEntity attacker, float damage);
-}
